a955188e0c00360e3b663d120b647325a36e590c
Ali Kazemini <kazemini.business@gmail.com>
2023-08-03 11:10:59 +0330

    add new scheme for web link scheme://host/page/data ...

--- lib/router/deep_router.dart
+++ lib/router/deep_router.dart
@@ -1,19 +1,29 @@
+import 'package:deep_link/pages/test_page.dart';
 import 'package:go_router/go_router.dart';
 
 import '../pages/home_page.dart';
 import '../pages/news_page.dart';
 
-final GoRouter router = GoRouter(routes: [
+final GoRouter router = GoRouter(
+    routes: [
   GoRoute(
     path: "/",
     builder: (context, state) => const HomePage(),
+    routes: [
+      GoRoute(
+        path: "news/:id/:path",
+        name: "news",
+        builder: (context, state) => NewsPage(
+          userId: state.pathParameters["id"].toString(),
+          path: state.pathParameters["path"].toString(),
+        ),
+      ),
+      GoRoute(
+        path: "test",
+        name: "test",
+        builder: (context, state) => const TestPage(),
+      )
+    ]
   ),
-  GoRoute(
-    path: "/news/:id/:path",
-    name: "news",
-    builder: (context, state) => NewsPage(
-      userId: state.pathParameters["id"].toString(),
-      path: state.pathParameters["path"].toString(),
-    ),
-  )
+
 ]);
